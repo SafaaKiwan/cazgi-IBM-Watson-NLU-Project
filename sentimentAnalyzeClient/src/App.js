0df2548f90b6e25f2bdae591881d7eb1e80a9a97
SafaaKiwan <safaakiwan@gmail.com>
2021-06-05 11:05:28 +0300

    Update App.js

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -55,12 +55,14 @@ class App extends React.Component {
       } else if (response.data === "negative"){
         output = <div style={{color:"red",fontSize:20}}>{response.data}</div>
       } else {
-        output = <div style={{color:"orange",fontSize:20}}>{response.data}</div>
+        output = <div style={{color:"yellow",fontSize:20}}>{response.data}</div>
       }
       this.setState({sentimentOutput:output});
     });
   }
-
+ componentDidMount(){
+    document.title = "Sentiment Analyzer"
+  }
   sendForEmotionAnalysis = () => {
     this.setState({sentiment:false});
     let ret = "";
